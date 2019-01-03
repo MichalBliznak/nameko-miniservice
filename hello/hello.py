3a0928dc1f1ead239a49bbc2a74842e597dcb90c
michalbliznak <michal.bliznak@gmail.com>
2019-01-03 16:08:06 +0100

    Add (random) service ID to the output message produced by the Hello service.

--- hello/hello.py
+++ hello/hello.py
@@ -7,9 +7,10 @@ import random
 
 class GreetingService:
     name = "greetings_service"
+    ID = random.randint(0, 1000)
 
     @rpc
     @timeout(5)
     def hello(self, name):
         time.sleep(random.randint(0, 10))
-        return "Hello, {}!".format(name)
+        return "Hello with ID '{}', {}!".format(GreetingService.ID, name)
