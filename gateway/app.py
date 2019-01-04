b988f0f95dcc25479e6357596e7e9a82cbdd3df8
michalbliznak <michal.bliznak@gmail.com>
2019-01-04 13:22:25 +0100

    Add login route.

--- gateway/app.py
+++ gateway/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 from flask_nameko import FlaskPooledClusterRpcProxy
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
+
 from auth import authorize, authorizations
 
 from dynaconf import settings
@@ -14,12 +15,21 @@ app.config.update(dict(NAMEKO_AMQP_URI=settings["rabbit_host"]))
 # Flask RESTplus setup
 api = Api(app, version='1.0', title='Sample API', description='A sample API', authorizations=authorizations)
 
+ns_login = api.namespace("login", description="Login API")
+ns_api = api.namespace("api", description="Features API")
+
 # Nameko setup
 rpc = FlaskPooledClusterRpcProxy()
 rpc.init_app(app)
 
+# Set up models
+login_model = api.model('Login', {
+    'user': fields.String(required=True, description='User name'),
+    'password': fields.String(required=True, description='User password')
+})
 
-@api.route('/hello/<name>')
+
+@ns_api.route('/hello/<name>')
 class HelloController(Resource):
     @api.doc(params={'name': 'Name to say Hello'})
     @api.doc(security="apikey")
@@ -32,3 +42,11 @@ class HelloController(Resource):
             message = "Ooops, the service seems to be unreachable... :("
         return {"message": message}
 
+
+@ns_login.route('/')
+class LoginController(Resource):
+    @api.doc(responses={403: "Forbidden", 200: "OK"})
+    @api.expect(login_model)
+    def post(self):
+        payload = request.json
+        return {"message": "Not implemented yet"}
