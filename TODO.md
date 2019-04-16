eaf99d5f5ceeb2d7801f68f4e752ad50c53f86af
michalbliznak <michal.bliznak@gmail.com>
2019-04-16 16:04:42 +0200

    Add initial support for RabbitMQ cluster.

--- TODO.md
+++ TODO.md
@@ -4,3 +4,4 @@ TODOs
 * Optionally move JWT secret to Vault server (https://dynaconf.readthedocs.io/en/latest/guides/sensitive_secrets.html)
 * Deploy in microk8s
 * Move credentials to Postgres
+* Use RabbitMQ HA cluster with Nameko microservices (https://medium.com/@oprearocks/set-up-a-rabbitmq-cluster-on-your-laptop-using-docker-4276555b0f28, https://groups.google.com/forum/#!topic/nameko-dev/Ohl_tf0YY1o)
---
+++ rabbitmq-slave/cluster-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Start RMQ from entry point.
+# This will ensure that environment variables passed
+# will be honored
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+
+# Do the cluster dance
+rabbitmqctl stop_app
+rabbitmqctl join_cluster rabbit@rabbit-master
+
+# Stop the entire RMQ server. This is done so that we
+# can attach to it again, but without the -detached flag
+# making it run in the foreground
+rabbitmqctl stop
+
+# Wait a while for the app to really stop
+echo "Sleeping for 5s..."
+sleep 5s
+
+# Start it
+rabbitmq-server
